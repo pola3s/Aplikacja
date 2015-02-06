6e3492097c3eed56439e1e1b77f294bf5d1f69c8
pola3s <paulina.serwinska@interia.pl>
2015-02-06 14:12:26 +0100

    Popprawne wyświetlanie danych w cart/show

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show]
 
+
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
