932a4ca7dbfefee5c66e132153d4d204a1506ceb
pola3s <paulina.serwinska@interia.pl>
2015-02-28 19:20:35 +0100

    Ostatnie poprawki

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,12 @@ class User < ActiveRecord::Base
 
   scope :koordynator, -> { where(role: 'Koordynator') }
 
-  validates_length_of :name, :minimum => 2
-  validates_length_of :surname, :minimum => 2
-  validates_length_of :phone, :minimum => 7
-  validates_length_of :city, :minimum => 2
-  validates :postcode, format: { with: /\A[0-9]{2}-[0-9]{3}\z/, message: "Niepoprawny kod pocztowy!" }
-  validates :email, format: { with: /\A[a-zA-Z0-9.\-_]+@[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,4}\z/, message: "Niepoprawny adres email pocztowy!" }
+  #validates_length_of :name, :minimum => 2
+  #validates_length_of :surname, :minimum => 2
+  #validates_length_of :phone, :minimum => 7
+  #validates_length_of :city, :minimum => 2
+  #validates :postcode, format: { with: /\A[0-9]{2}-[0-9]{3}\z/, message: "Niepoprawny kod pocztowy!" }
+  #validates :email, format: { with: /\A[a-zA-Z0-9.\-_]+@[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,4}\z/, message: "Niepoprawny adres email!" }
 
 
   #before_save :default_role
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,16 +37,16 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: "paulina.serwinska@gmail.com",
-      password: "123"
-  }
+  #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = {
+  #    address: "smtp.gmail.com",
+  #    port: 587,
+  #    authentication: "plain",
+  #    enable_starttls_auto: true,
+  #    user_name: " ",
+  #    password: " "
+  #}
 
 
 
